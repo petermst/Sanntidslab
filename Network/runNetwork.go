a8dde4a0af2fbc41d45fbe0d40d27c14eab9575f
tordsb <tord.bjornsen@gmail.com>
2017-02-16 14:45:10 +0100

    Upload of runNetwork

---
+++ Network/runNetwork.go
@@ -0,0 +1,29 @@
+package runNetwork
+
+import (
+	"./bcast"
+	"./localip"
+	"./peers"
+	"fmt"
+	"os"
+	"time"
+)
+
+func runNetwork(){
+	var id string
+	if id == "" {
+		localIP, err := localip.LocalIP()
+		if err != nil {
+
+		}
+	}
+}
+
+func sendMessage(){
+
+}
+
+func receiveMessage(){
+
+}
+
